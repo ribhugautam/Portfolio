a2c011dcc15ecee0a672e3dcedbe307495551aa9
ribhugautam <gautamribhu@gmail.com>
2024-11-01 13:08:47 +0530

    Fixed typescript error

--- src/app/(root)/projects/[...id]/page.tsx
+++ src/app/(root)/projects/[...id]/page.tsx
@@ -5,20 +5,43 @@ import { MdArrowBackIosNew } from "react-icons/md";
 
 interface Project {
   id: number;
+  name: string;
+  image: string;
+  link: string;
+  description: string;
 }
 
-async function page({ params }: { params: { id: string } }) {
-  const { id } = await params;
-  const project = data.projects;
+interface Params {
+  params: {
+    id: string;
+  };
+}
+
+async function Page({ params }: Params) {
+  const { id } = params;
+  const project: Project[] = data.projects;
   const filteredProject = project.filter((p: Project) => p.id === Number(id));
+
+  if (filteredProject.length === 0) {
+    return (
+      <div className="w-11/12 min-h-screen font-Caveat max-w-[80%] mx-auto">
+        {" "}
+        Project not found
+      </div>
+    );
+  }
+
   return (
     <div className="pattern h-dvh">
       <div className="w-11/12 min-h-screen font-Caveat max-w-[80%] mx-auto">
         <div className="pt-28 mx-auto sm:pt-36 lg:pt-32">
           <section className="flex flex-col p-6 justify-center items-center gap-12">
             <div className="relative w-full">
-              <Link href="/projects" className="text-4xl mt-2 flex justify-center items-center text-violet-600 absolute left-0 ">
-                <MdArrowBackIosNew /> 
+              <Link
+                href="/projects"
+                className="text-4xl mt-2 flex justify-center items-center text-violet-600 absolute left-0"
+              >
+                <MdArrowBackIosNew />
               </Link>
               <h1 className="text-5xl mb-12 font-bold pb-6 font-Caveat underline decoration-violet-600 underline-offset-4 text-black text-center">
                 {filteredProject[0].name}
@@ -46,4 +69,4 @@ async function page({ params }: { params: { id: string } }) {
   );
 }
 
-export default page;
+export default Page;
