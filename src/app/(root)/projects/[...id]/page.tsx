f6f97a1b8c823dace4998c6472f4324d380ab737
ribhugautam <gautamribhu@gmail.com>
2024-11-01 17:19:21 +0530

    Fixed next js issue

--- src/app/(root)/projects/[...id]/page.tsx
+++ src/app/(root)/projects/[...id]/page.tsx
@@ -3,18 +3,29 @@ import data from "../../../../lib/data.json";
 import Link from "next/link";
 import { MdArrowBackIosNew } from "react-icons/md";
 
-async function page({ params }: { params: { id: number} }) {
+type Params = Promise <{ id: number }>;
+
+export async function generateStaticParams({ params }: { params: Params }) {
+  const { id } = await params;
+}
+
+export default async function page({ params }: { params: Params }) {
   const { id } = await params;
   const project = data.projects;
-  const filteredProject = project.filter((project: { id: number} ) => project.id == id);
+  const filteredProject = project.filter(
+    (project: { id: number }) => project.id == id
+  );
   return (
     <div className="pattern h-dvh">
       <div className="w-11/12 min-h-screen font-Caveat max-w-[80%] mx-auto">
         <div className="pt-28 mx-auto sm:pt-36 lg:pt-32">
           <section className="flex flex-col p-6 justify-center items-center gap-12">
             <div className="relative w-full">
-              <Link href="/projects" className="text-4xl mt- flex justify-center items-center text-violet-600 absolute left-0 ">
-                <MdArrowBackIosNew /> 
+              <Link
+                href="/projects"
+                className="text-4xl mt- flex justify-center items-center text-violet-600 absolute left-0 "
+              >
+                <MdArrowBackIosNew />
               </Link>
               <h1 className="text-5xl mb-12 font-bold pb-6 font-Caveat underline decoration-violet-600 underline-offset-4 text-black text-center">
                 {filteredProject[0].name}
@@ -41,5 +52,3 @@ async function page({ params }: { params: { id: number} }) {
     </div>
   );
 }
-
-export default page;
