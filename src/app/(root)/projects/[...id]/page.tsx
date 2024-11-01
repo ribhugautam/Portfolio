4ce13fa022c5b9a897c8af03d1b1a864c614a42e
ribhugautam <gautamribhu@gmail.com>
2024-11-01 17:10:18 +0530

    fixed errors for type

--- src/app/(root)/projects/[...id]/page.tsx
+++ src/app/(root)/projects/[...id]/page.tsx
@@ -3,38 +3,17 @@ import data from "../../../../lib/data.json";
 import Link from "next/link";
 import { MdArrowBackIosNew } from "react-icons/md";
 
-interface Project {
-  id: number;
-  name: string;
-  image: string;
-  link: string;
-  description: string;
-}
-
-interface Params {
-  id: string;
-}
-
-interface PageProps {
-  params: Params;
-}
-
-const Page = async ({ params }: PageProps) => {
+async function page({ params }: { params: { id: number} }) {
   const { id } = await params;
-  const project: Project[] = data.projects;
-  const filteredProject = project.filter((p) => p.id === Number(id));
-
-  if (filteredProject.length === 0) {
-    return <div>Project not found</div>;
-  }
-
+  const project = data.projects;
+  const filteredProject = project.filter((project: { id: number} ) => project.id == id);
   return (
     <div className="pattern h-dvh">
       <div className="w-11/12 min-h-screen font-Caveat max-w-[80%] mx-auto">
         <div className="pt-28 mx-auto sm:pt-36 lg:pt-32">
           <section className="flex flex-col p-6 justify-center items-center gap-12">
             <div className="relative w-full">
-              <Link href="/projects" className="text-4xl mt-2 flex justify-center items-center text-violet-600 absolute left-0">
+              <Link href="/projects" className="text-4xl mt- flex justify-center items-center text-violet-600 absolute left-0 ">
                 <MdArrowBackIosNew /> 
               </Link>
               <h1 className="text-5xl mb-12 font-bold pb-6 font-Caveat underline decoration-violet-600 underline-offset-4 text-black text-center">
@@ -44,9 +23,7 @@ const Page = async ({ params }: PageProps) => {
             <img
               src={filteredProject[0].image}
               alt={filteredProject[0].name}
-              className="object-cover outline rounded-lg scale-125"
-              width={500} // Specify a width
-              height={500} // Specify a height
+              className="object-cover outline rounded-lg scale-125 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
             />
             <Link
               href={filteredProject[0].link}
@@ -63,6 +40,6 @@ const Page = async ({ params }: PageProps) => {
       </div>
     </div>
   );
-};
+}
 
-export default Page;
+export default page;
